085b594ae082b0ab46ef5a2cb2a06e26dabd3a72
beatrixyu <beatrixyu@126.com>
2020-07-05 18:20:04 +0200

    Updates

---
+++ precache-manifest.14718ef9f03d3076d97676209683cee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003a0aa90dce29759777535010a9525e",
+    "url": "/deutsch-bahn-info-graphql/index.html"
+  },
+  {
+    "revision": "621b314a77e047373fa7",
+    "url": "/deutsch-bahn-info-graphql/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2eea03c532039b45d19e",
+    "url": "/deutsch-bahn-info-graphql/static/js/2.1140cf2e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/deutsch-bahn-info-graphql/static/js/2.1140cf2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621b314a77e047373fa7",
+    "url": "/deutsch-bahn-info-graphql/static/js/main.8272a472.chunk.js"
+  },
+  {
+    "revision": "c24eee73c55c2454debb",
+    "url": "/deutsch-bahn-info-graphql/static/js/runtime-main.2f414c25.js"
+  }
+]);
